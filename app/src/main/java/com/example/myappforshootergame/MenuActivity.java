0a81473cc44196da2298b4b60d8f46b707e53340
Héctor Fernández <43781682+hectorfdezg@users.noreply.github.com>
2019-01-17 09:45:05 +0100

    Subo la estructura App a GitHub falta implementaciones

---
+++ app/src/main/java/com/example/myappforshootergame/MenuActivity.java
@@ -0,0 +1,50 @@
+package com.example.myappforshootergame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    Button btnExit;
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnExit = (Button) findViewById(R.id.btn_exit);
+
+
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*iniciar apk*/
+
+            }
+        });
+
+
+    }
+}
